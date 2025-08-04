13d646746ced1120e82662cc3b2da54107be8c62
gueon7866 <lsg7866@gmail.com>
2025-08-04 16:35:12 +0900

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,9 @@
+    const swiper = new Swiper(".carousel-slider", {
+      slidesPerView: 3,
+      spaceBetween: 30,
+      loop:true,
+      centeredSlides: true,
+      autoplay:true,
+      speed:5     
+
+    });
